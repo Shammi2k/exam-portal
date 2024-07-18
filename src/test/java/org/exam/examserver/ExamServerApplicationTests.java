4777dfe1b190d9156b104fc3177a3aa6134e0488
Shammi2k <shammi2k@gmail.com>
2024-07-18 17:52:26 +0530

    First commit of basic Springboot web application and database configuration

---
+++ src/test/java/org/exam/examserver/ExamServerApplicationTests.java
@@ -0,0 +1,15 @@
+package org.exam.examserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamServerApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
