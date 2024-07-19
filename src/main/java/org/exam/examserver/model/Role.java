04faa31f797d62f8aa404f60bcc35c3c56560143
Shammi2k <shammi2k@gmail.com>
2024-07-19 20:00:56 +0530

    Added entities for user, role and userRole

---
+++ src/main/java/org/exam/examserver/model/Role.java
@@ -0,0 +1,62 @@
+package org.exam.examserver.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity(name = "roles")
+public class Role
+{
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String name;
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "role")
+  private Set<UserRole> userRoles = new HashSet<>();
+
+  public Role()
+  {
+  }
+
+  public Role(Long id, String name)
+  {
+    this.id = id;
+    this.name = name;
+  }
+
+  public Long getId()
+  {
+    return id;
+  }
+
+  public void setId(Long id)
+  {
+    this.id = id;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public Set<UserRole> getUserRoles()
+  {
+    return userRoles;
+  }
+
+  public void setUserRoles(Set<UserRole> userRoles)
+  {
+    this.userRoles = userRoles;
+  }
+}
---
+++ src/main/java/org/exam/examserver/model/UserRole.java
@@ -0,0 +1,55 @@
+package org.exam.examserver.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class UserRole
+{
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  //User mapping. Fetch type defaults to EAGER
+  @ManyToOne
+  private User user;
+  //Role mapping. Fetch type defaults to EAGER
+  @ManyToOne
+  private Role role;
+
+  public UserRole()
+  {
+  }
+
+  public Long getId()
+  {
+    return id;
+  }
+
+  public void setId(Long id)
+  {
+    this.id = id;
+  }
+
+  public User getUser()
+  {
+    return user;
+  }
+
+  public void setUser(User user)
+  {
+    this.user = user;
+  }
+
+  public Role getRole()
+  {
+    return role;
+  }
+
+  public void setRole(Role role)
+  {
+    this.role = role;
+  }
+}
